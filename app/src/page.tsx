8ba2dbb47ea3c8edc33aa9af74e22a57b559b8a8
bowl <harshith10295032@gmail.com>
2024-08-20 16:31:20 +0530

    project

---
+++ app/src/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import TextareaAutosize from "react-textarea-autosize";
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
+import './globals.css';
+
+export default function Home() {
+  const Editor = useMemo(
+    () => dynamic(() => import('@/app/components/Editior'), { ssr: false }), []
+  );
+  return (
+    <main className="min-h-screen bg-[#0c0c0c]">
+      <div className="flex flex-col px-24 py-10 w-full h-screen">
+        {/* <TextareaAutosize
+          placeholder=" Title"
+          className="w-full resize-none overflow-hidden appearance-none text-6xl bg-[#0c0c0c] placeholder-[#7F7F7F] my-17"
+        /> */}
+        <Editor onChange={() => {}} />
+          
+      </div>
+    </main>
+  );
+}
